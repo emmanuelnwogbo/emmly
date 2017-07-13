dccf4cd6a64a82ef17231b9db95f831420f79384
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-07-13 20:47:40 +0000

    updated config files to use heroku mongodb url

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  database: "mongodb://localhost:27017/emmly",
+  database: process.env.DATABASE || 'mongodb://heroku_pnhdn64m:62pocm1jd01onqtrecao8osheh@ds151222.mlab.com:51222/heroku_pnhdn64m',
   port: process.env.PORT || 3000,
   secret: process.env.SECRET || 'yabadabadoo',
 
